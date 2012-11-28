c5e5cb08d5b72393f962e1537aeb1cd4e74d4c3e
Adam Mulligan <adammulligan@gmail.com>
2012-11-28 19:46:29 +0000

    Fixed MBTiles TileLayer extension to work with leaflet 0.45; added ability to draw polygons (admittedly fairly crudely) on touch devices

--- www/js/TileLayer.MBTiles.js
+++ www/js/TileLayer.MBTiles.js
@@ -1,44 +1,44 @@
+/**
+ * Leaflet TileLayer extension for displaying tiles stored
+ * in the .mbtiles format.
+ */
 L.TileLayer.MBTiles = L.TileLayer.extend({
-	db: null,
-
-	initialize: function(url, options, db) {
+    initialize: function(db, options) {
 		this.db = db;
-
 		L.Util.setOptions(this, options);
 	},
-	getTileUrl: function (tilePoint, zoom, tile) {
-		var z = this._getOffsetZoom(zoom);
-		var x = tilePoint.x;
-		var y = tilePoint.y;
-		var base64Prefix = 'data:image/gif;base64,';
-
-        this.db.transaction(function(tx) {
-            tx.executeSql(
-                "SELECT tile_data FROM images " +
-                "INNER JOIN map ON images.tile_id = map.tile_id " +
-                "WHERE zoom_level = ? " +
-                "AND tile_column = ? " +
-                "AND tile_row = ?",
-//                "SELECT tile_data FROM tiles " +
-//                "WHERE zoom_level = ? " +
-//                "AND tile_column = ? " +
-//                "AND tile_row = ?",
-                [z, x, y],
-                function (tx, res) {
-                    tile.src = base64Prefix + res.rows.item(0).tile_data;
-                },
-                function (er) {
-                    console.log('error with executeSql', er);
-                }
-            );
-        });
-
-
-	},
-	_loadTile: function (tile, tilePoint, zoom) {
-		tile._layer = this;
-		tile.onload = this._tileOnLoad;
-		tile.onerror = this._tileOnError;
-		this.getTileUrl(tilePoint, zoom, tile);
-	}
+  
+  _loadTile: function(tile, tilePoint) {
+    this._adjustTilePoint(tilePoint);
+    
+    tile._layer = this;
+    tile.onload = this._tileOnLoad;
+    tile.onerror = this._tileOnError;
+    
+    var z = this._getZoomForUrl();
+    var x = tilePoint.x;
+    var y = tilePoint.y;
+    var base64Prefix = 'data:image/gif;base64,';
+    
+    tile.src = base64Prefix;
+    this.db.transaction(function(tx) {
+      tx.executeSql(
+        "SELECT tile_data FROM images " +
+        "INNER JOIN map ON images.tile_id = map.tile_id " +
+        "WHERE zoom_level = ? " +
+        "AND tile_column = ? " +
+        "AND tile_row = ?",
+        [z, x, y],
+        function (tx, res) {
+          if (res.rows.length > 0) {
+            tile.src += res.rows.item(0).tile_data;
+          }
+        },
+        function (er) {
+          console.log('Database Error');
+          console.log(er);
+        }
+      );
+    });
+  }
 });
--- www/js/index.js
+++ www/js/index.js
@@ -1,10 +1,15 @@
-document.addEventListener("deviceready", init, false);
+window.onerror = function(error, file, line) {
+  console.log('gerror');
+  console.log(error);
+  console.log(file);
+  console.log(line);
+};
 
-var localFileName = 'test.mbtiles';
+var localFileName = 'bluecarbon.mbtiles';
 var remoteFile = 'https://dl.dropbox.com/s/jllnjvlwvhfqys1/test.mbtiles?dl=1';
 var msg;
 
-function init() {
+var init = function() {
 	var fs;
 	var ft;
 
@@ -18,7 +23,7 @@ function init() {
 
 			buildMap();
 		}, function () {
-            msg.innerHTML = 'Downloading file...';
+      msg.innerHTML = 'Downloading file...';
 
 			ft = new FileTransfer();
 			ft.download(remoteFile, fs.root.fullPath + '/' + localFileName, function (entry) {
@@ -30,17 +35,22 @@ function init() {
 	});
 }
 
-function buildMap() {
+var buildMap = function() {
 	var db = window.sqlitePlugin.openDatabase(localFileName, "1.0", "Tiles", 2000000);
 
 	document.body.removeChild(msg);
-
+    
 	var map = new L.Map('map', {
-		center: new L.LatLng(40.6681, -111.9364),
-		zoom: 11
+		center: new L.LatLng(24.2870, 54.3274),
+		zoom: 9
 	});
-
-	var lyr = new L.TileLayer.MBTiles('', {maxZoom: 14, scheme: 'tms'}, db);
-
-	map.addLayer(lyr);
+  
+  tileLayer = new L.TileLayer.MBTiles(db, {
+    tms: true
+  }).addTo(map);
+  
+  var polygonDraw = new L.Polygon.Draw(map, {});
+  polygonDraw.enable();
 }
+
+document.addEventListener("deviceready", init, false);
